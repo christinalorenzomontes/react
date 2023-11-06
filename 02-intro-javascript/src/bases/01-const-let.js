5a8e04b5a367336cab8b9dc233510740550d9575
Christina Lorenzo <christinalorenzo@gmail.com>
2023-11-06 13:22:12 +0100

    add first practise

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,18 @@
+
+// Variables y Constantes
+
+const name = 'Christina';
+const surname = 'Lorenzo';
+
+let givenValue = 5;
+givenValue = 4;
+
+console.log(name, surname, givenValue);
+
+// var no se debe usar ya
+if ( true ) {
+    const name = 'Krysza';
+    console.log (name)
+}
+
+console.log( givenValue );
