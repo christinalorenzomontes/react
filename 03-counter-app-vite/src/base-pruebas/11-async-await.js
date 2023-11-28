b38b55e8d26b50d70db54f16c349abe6d555d620
Christina Lorenzo <christinalorenzo@gmail.com>
2023-11-28 13:18:15 +0100

    testing catch

--- 03-counter-app-vite/src/base-pruebas/11-async-await.js
+++ 03-counter-app-vite/src/base-pruebas/11-async-await.js
@@ -3,7 +3,7 @@ export const getImagen = async() => {
 
     try {
 
-        const apiKey = 'rmKHcEx9HqMUxZ1ubUVugb1mTOdVmiO0';
+        const apiKey = '';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
--- 03-counter-app-vite/src/tests/base-pruebas/11-async-await.test.js
+++ 03-counter-app-vite/src/tests/base-pruebas/11-async-await.test.js
@@ -2,14 +2,12 @@ import { getImagen } from "../../base-pruebas/11-async-await";
 
 describe('Pruebas en 11-async-await.js', () => {
     
-    test('getImagen debe de retornar un error si no tenemos api key', async() => {
+    test('getImagen debe retornar un error si no tenemos api key', async() => {
         
         const resp = await getImagen();
         // expect( typeof url ).toBe('string');
         expect( resp ).toBe('No se encontro la imagen');
 
     });
-
-
 
 });
