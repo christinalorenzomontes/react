0516d2a5f48ef130b2959f4f97bf823abe0926df
Christina Lorenzo <christinalorenzo@gmail.com>
2023-11-28 12:23:53 +0100

    tests with import and export

---
+++ 03-counter-app-vite/src/tests/base-pruebas/08-imp-exp.test.js
@@ -0,0 +1,35 @@
+import { getHeroeById, getHeroesByOwner } from "../../base-pruebas/08-imp-exp";
+
+describe('Pruebas en 08-imp-exp', () => {
+    
+    test('getHeroeByIde debe retornar un héroe por ID', () => {
+        
+        const id = 1;
+        const hero = getHeroeById( id );
+        
+        expect( hero ).toEqual({ id: 1, name: 'Batman', owner: 'DC' });
+    })
+
+    test('getHeroeById debe retornar undefined si no existe el id', () => {
+        
+        const id = 100;
+        const hero = getHeroeById( id );
+        
+        expect( hero ).toBeFalsy();
+    })
+
+    // Tarea 
+    test('getHeroesByOwner debe retornar los héroes según la editorial', () => {
+        
+        const ownerName = 'DC';
+        const heroByOwner = getHeroesByOwner( ownerName );
+        
+        expect( heroByOwner.length ).toBe( 3 );
+        expect( heroByOwner ).toEqual([
+            { id: 1, name: 'Batman', owner: 'DC' },
+            { id: 3, name: 'Superman', owner: 'DC' },
+            { id: 4, name: 'Flash', owner: 'DC' },
+        ]);
+
+    })
+})
